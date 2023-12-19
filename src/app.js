d4cd375291cb2f4840ccad85d4dccd9b3e157f58
NicolasMamani <niko_mamani@hotmail.com>
2023-12-19 17:12:03 -0300

    subiendo desafio 3

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const PUERTO = 8080;
 const express = require("express");
-const { ProductManager } = require("./ProductManager");
+const ProductManager = require("./ProductManager");
 
 //creamos productos
 
@@ -9,13 +9,14 @@ const manager = new ProductManager("./src/productos.json");
 // creamos el servidor
 const app = express();
 
+// get para productos con limit
 app.get("/products", async (req, res) => {
   try {
     const limite = parseInt(req.query.limit);
     const products = await manager.leerArchivo();
     if (limite) {
       let arrayConLimite = [];
-      for (let i = 1; i <= limite; i++) {
+      for (let i = 0; i < limite; i++) {
         arrayConLimite.push(products[i]);
       }
       return res.send(arrayConLimite);
@@ -26,6 +27,21 @@ app.get("/products", async (req, res) => {
   }
 });
 
+// get para un producto especifico
+app.get("/products/:pid", async (req, res) => {
+  try {
+    const id = parseInt(req.params.pid);
+    const encontrado = await manager.getProductById(id);
+    if (encontrado && encontrado != "Producto no encontrado") {
+      return res.send(encontrado);
+    } else {
+      res.status(404).json({ error: "El producto no existe" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
 app.listen(PUERTO, () => {
-  console.log("Escuchando en el http://localhost:8080/");
+  console.log("Escuchando en el http://localhost:8080/products");
 });
